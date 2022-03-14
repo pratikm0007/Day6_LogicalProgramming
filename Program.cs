b3de8d31a876808246ad4661a53175354955a1ce
pratikm0007 <pratikpmondal07@gmail.com>
2022-03-14 09:16:26 +0530

    Push febonacci series file

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BasicLogicalProgramming
+{
+    public class Program
+    {
+        //Fibonacci series 
+
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+            int c;
+
+            Console.Write("Enter number to get febbonaci series : ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Febonnacci Series : " + a +" " + b);
+
+            for (int i = 0; i < N; i++)
+            {
+                c = a + b;
+                Console.Write(" "+c);
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
