80b25a73332156f052da9d50a1ea400e787b5b22
pratikm0007 <pratikpmondal07@gmail.com>
2022-03-14 13:53:07 +0530

    Add coupon file

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BasicLogicalProgramming
+{
+    public class Program
+    {
+        //Generate a random Coupon. 
+
+        static void Main(string[] args)
+        {
+            var nDistinct = "0123456789";
+
+            var stringChars = new char[6];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = nDistinct[random.Next(nDistinct.Length)];
+            }
+
+            var finalCoupon = new String(stringChars);
+            Console.WriteLine("Generated Coupon number is "+finalCoupon);
+        }
+    }
+}
