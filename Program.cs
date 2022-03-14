c22377417e607d2596735224bfbf14403014f9ca
pratikm0007 <pratikpmondal07@gmail.com>
2022-03-14 13:09:26 +0530

    Push Reverse Number file

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BasicLogicalProgramming
+{
+    public class Program
+    {
+        //Reverse the number. 
+
+        static void Main(string[] args)
+        {
+            int number, remainder;
+            int reverse = 0;
+
+            Console.Write("Enter a number to reverse it: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            while (number != 0)
+            {
+                remainder = number % 10;
+                reverse = reverse * 10 + remainder;
+                number /= 10;
+            }
+            Console.Write("Reversed Number is: " + reverse);
+        }
+
+    }
+}
