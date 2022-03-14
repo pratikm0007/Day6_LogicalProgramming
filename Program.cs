2b8ad3cb938563724805bf08f10728430325e132
pratikm0007 <pratikpmondal07@gmail.com>
2022-03-14 14:53:37 +0530

    Add stopWatch file

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+class Program
+{
+    //StopWatch
+    static void Main(string[] args)
+    {
+        
+        Stopwatch stopwatch = new Stopwatch();
+
+        // Start time
+        Console.WriteLine("Start by Press 1");
+        int start = Convert.ToInt32(Console.ReadLine());
+        stopwatch.Start();
+
+        Thread.Sleep(1);
+
+        // Stop time.
+        Console.WriteLine("Stop by Press 1");
+        int stop = Convert.ToInt32(Console.ReadLine());
+        stopwatch.Stop();
+
+        Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+    }
+}
